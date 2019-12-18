3d2b8c469716f4d5e48b1395c937c30d4d5142d7
permenent <2691556679@qq.com>
2019-12-18 22:25:42 +0800

    commit

---
+++ 748_找两个字符串中的公共字符.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <iostream>
+#include <string>
+using namespace std;
+
+//string V_compare(string& s1, string& s2)
+//{
+//	string  str;
+//	for (int i = 0; i < s1.size(); i++)
+//	{
+//		for (int j = 0; j < s2.size(); j++)
+//		{
+//			if (s1[i] == s2[j])
+//			{
+//				str += s1[i];
+//				s2.erase(j,1);
+//				break;
+//			}
+//		}
+//	}
+//	return str;
+//}
+////bool V_compare(string& s1, string& s2)
+////{
+////	for (int i = 0; i < s1.size(); i++)
+////	{
+////		for (int j = 0; j < s2.size(); j++)
+////		{
+////			if (s1[i] == s2[j])
+////			{
+////				s2.erase(j, 1);
+////				j -= 1;
+////				break;
+////			}
+////		}
+////	}
+////	if (s2.empty())
+////		return true;
+////	return false;
+////}
+//int main()
+//{
+//	string str1("old");
+//	string str2("ah");
+//	cout << V_compare(str1,str2) << endl;
+//	system("pause");
+//}
+
