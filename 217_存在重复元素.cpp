44110a103da3859faa130c9ee9061a47085a381f
permenent <2691556679@qq.com>
2020-01-01 21:02:13 +0800

    commit

---
+++ 217_存在重复元素.cpp
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS 1 
+//
+//#include <iostream>  
+//#include <vector>
+//using namespace std;
+
+//AC²»¹ý      3^1  == 0
+//bool containsDuplicate(vector<int>& nums) {
+//	if (nums.empty())
+//		return false;
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//		for (int j = i + 1; j < nums.size(); j++)
+//		{
+//			if (nums[i]^nums[j] == 0)
+//				return true;
+//		}
+//	}
+//	return false;
+//}
+//int main()
+//{
+//	vector<int> v;
+//	v.push_back(3);
+//	v.push_back(1);
+//	cout << containsDuplicate(v) << endl;
+//	system("pause");
+//}
