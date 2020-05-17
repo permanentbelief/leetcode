f497dfedff08eaa5124a0f67ae64639b1ce37397
permenent <2691556679@qq.com>
2020-05-17 22:38:35 +0800

    commit

---
+++ 51_数组中的逆序对.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int mergeSort(vector<int>& nums, vector<int>& tmp, int l, int r) {
+		if (l >= r) {
+			return 0;
+		}
+
+		int mid = (l + r) / 2;
+		int inv_count = mergeSort(nums, tmp, l, mid) + mergeSort(nums, tmp, mid + 1, r);
+		int i = l, j = mid + 1, pos = l;
+		while (i <= mid && j <= r) {
+			if (nums[i] <= nums[j]) {
+				tmp[pos] = nums[i];
+				++i;
+				inv_count += (j - (mid + 1));
+			}
+			else {
+				tmp[pos] = nums[j];
+				++j;
+			}
+			++pos;
+		}
+		for (int k = i; k <= mid; ++k) {
+			tmp[pos++] = nums[k];
+			inv_count += (j - (mid + 1));
+		}
+		for (int k = j; k <= r; ++k) {
+			tmp[pos++] = nums[k];
+		}
+		copy(tmp.begin() + l, tmp.begin() + r + 1, nums.begin() + l);
+		return inv_count;
+	}
+
+	int reversePairs(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> tmp(n);
+		return mergeSort(nums, tmp, 0, n - 1);
+	}
+};
+int main()
+{
+	Solution s;
+	vector<int> nums = { 7, 5, 6, 4 };
+	cout << s.reversePairs(nums) << endl;
+
+	system("pause");
+}
