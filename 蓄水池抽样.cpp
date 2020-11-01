c40d81fe9aa6c0480ad28f289d7f83db144bc3bb
permenent <2691556679@qq.com>
2020-11-01 22:08:12 +0800

    commit

---
+++ 蓄水池抽样.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+//
+//
+//vector<int> res = { 1, 3, 4, 2, 2,3,3};
+//int pick(int target) {
+//	int c = 0;
+//	int index = 0;
+//	for (int i = 0; i < res.size(); i++)
+//	{
+//		if (res[i] == target)
+//		{
+//			c++;
+//			if (rand() % c == 0) index = i;
+//		}
+//	}
+//	return index;
+//}
+//int main()
+//{
+//
+//	cout << pick(3) << endl;
+//
+//	system("pause");
+//}
